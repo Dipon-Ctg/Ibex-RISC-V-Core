4d98ff06ab7b0594be04f829e04e38e094509d33
Dipon Barua <85799541+Dipon-Ctg@users.noreply.github.com>
2024-08-10 01:15:24 +0200

    Delete syn/1st_syn.cmd

--- syn/1st_syn.cmd
+++
@@ -1,9 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 21.14-s082_1, built Jun 23 2022 14:32:08
-
-# Date: Fri Aug 09 18:33:23 2024
-# Host: item0110.item.uni-bremen.de (x86_64 w/Linux 3.10.0-1160.119.1.el7.x86_64) (6cores*24cpus*2physical cpus*Intel(R) Xeon(R) CPU X5650 @ 2.67GHz 12288KB)
-# OS:   CentOS Linux release 7.9.2009 (Core)
-
-source genus.tcl
-gui_show
-exit
