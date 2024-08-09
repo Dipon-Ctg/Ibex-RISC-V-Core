9f5c52112aebb10f371602ac4bbe43aa08a7f0fa
Dipon Barua <85799541+Dipon-Ctg@users.noreply.github.com>
2024-08-10 01:23:39 +0200

    Delete syn/fv/ibex_core directory

--- syn/fv/ibex_core/read_libs.tcl
+++
@@ -1,4 +0,0 @@
-add_search_path /usrf01/prog/tsmc/40/cmos/lp/digital/Front_End/timing_power_noise/NLDM/tcbn40lpbwp12t40m1p_200a -library -both
-read_library -liberty -both \
-    /usrf01/prog/tsmc/40/cmos/lp/digital/Front_End/timing_power_noise/NLDM/tcbn40lpbwp12t40m1p_200a/tcbn40lpbwp12t40m1pwc.lib
-
